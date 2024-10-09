115596cd6ce1a5d8dd9aacb29ec6907b686d3706
test <test@gmail.com>
2024-10-09 19:46:10 +0300

    Add project files.

---
+++ shop_app/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shop_app.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        //Register
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Register
+        [HttpPost]
+        public async Task<IActionResult> Create(string email, string password)
+        {
+            if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest("Email or password are error!");
+            }
+            var newUser = new IdentityUser
+            {
+                Email = email,
+                UserName = email,
+                EmailConfirmed = true //Imitation check email
+            };
+            var result = await _userManager.CreateAsync(newUser, password);
+            if(result.Succeeded)
+            {
+                return Ok("User is registered successfully");
+            }
+            return BadRequest("Error register!");
+        }
+        //auth
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            Console.WriteLine($"Login: {email}, password: {password}");
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest("Email or password are error!");
+            }
+            var result = await _signInManager.PasswordSignInAsync(
+                email, 
+                password,
+                isPersistent: false,
+                lockoutOnFailure: false
+                );
+            if (result.Succeeded)
+            {
+                //HttpContext.Session.SetString("login", email);
+                //return Ok("The user is authorized ...");
+                return Redirect("~/Home/Index");
+            }
+            return BadRequest("Error auth ...");
+        }
+    }
+}
---
+++ shop_app/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace shop_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<ProductContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddDbContext<UserContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}

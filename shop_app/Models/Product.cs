3b237ae89043252b1a662d349665f865100c126a
test <test@gmail.com>
2024-10-23 21:14:13 +0300

    valid create

--- shop_app/Models/Product.cs
+++ shop_app/Models/Product.cs
@@ -9,14 +9,15 @@ namespace shop_app.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; } = 0;
-        [Required]
+        [Required(ErrorMessage = "Name is required ...")]
         [StringLength(20, MinimumLength = 2, ErrorMessage = "min: 2, max: 20")]
         public string? Name { get; set; } = string.Empty;
-        [Required]
         [Precision(10, 2)]
+        [Required(ErrorMessage = "Price is required ...")]
+        [Range(0.01, 100000.00, ErrorMessage = "min: 0.01, max: 100000.00")]
         public decimal Price { get; set; } = decimal.Zero;
-        [Required]
-        [StringLength(1024)]
+        [Required(ErrorMessage = "Description is required ...")]
+        [StringLength(1024, MinimumLength = 2, ErrorMessage = "min: 2, max: 1024")]
         public string? Description { get; set; } = string.Empty;
     }
 }
